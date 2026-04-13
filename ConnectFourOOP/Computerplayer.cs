b320420e23d5ca2bf0b90c7a68a2ce72f27ec515
Sweeny Elli Tan <sweenyelli@gmail.com>
2026-04-13 00:25:32 -0600

    Add files via upload

---
+++ ConnectFourOOP/Computerplayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConnectFourOOP
+{
+    using System;
+
+    class ComputerPlayer : Player
+    {
+        private Random rand = new Random();
+
+        public ComputerPlayer(char symbol) : base(symbol) { }
+
+        public override int GetMove()
+        {
+            return rand.Next(0, 7);
+        }
+    }
+}
