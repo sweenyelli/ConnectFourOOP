725fa62be536eb9423cc065b9ba6a6b305112239
Sweeny Elli Tan <sweenyelli@gmail.com>
2026-04-15 16:27:55 -0600

    Add files via upload

--- ConnectFourOOP/Player.cs
+++ ConnectFourOOP/Player.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace ConnectFourOOP
 {
-    internal class Player
+    abstract class Player
     {
+        public char Symbol { get; set; }
+
+        public Player(char symbol)
+        {
+            Symbol = symbol;
+        }
+
+        public abstract int GetMove();
     }
 }
--- ConnectFourOOP/RealPlayer.cs
+++ ConnectFourOOP/RealPlayer.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace ConnectFourOOP
 {
-    internal class RealPlayer
+    using System;
+
+    class HumanPlayer : Player
     {
+        public HumanPlayer(char symbol) : base(symbol) { }
+
+        public override int GetMove()
+        {
+            Console.Write("Enter column (1-7): ");
+            int col = int.Parse(Console.ReadLine());
+            return col - 1;
+        }
     }
 }
